a7cbf7469db17ece211a8053404ffc4eb41b57a6
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-23 18:58:41 +0530

    Added bcrypt, hashed password

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cryptoSchema = require('./Crypto');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -20,5 +21,38 @@ const userSchema = new mongoose.Schema({
   //   _id: false,
   // },
 });
+
+const SALT_FACTOR = 10;
+
+userSchema.pre('save', function (done) {
+  const user = this;
+
+  // password has not been updated
+  if (!user.isModified('password')) {
+    return done();
+  }
+
+  // password has been updated - hash and save it
+  bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+    if (err) {
+      return done(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hashedPassword) => {
+      if (err) {
+        return done(err);
+      }
+
+      user.password = hashedPassword;
+      done();
+    });
+  });
+});
+
+userSchema.methods.checkPassword = function (password, done) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    done(err, isMatch);
+  });
+};
 
 mongoose.model('User', userSchema);
