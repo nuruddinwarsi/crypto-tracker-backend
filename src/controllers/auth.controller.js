5d902ad8164d6476f6611cb133dcd1d6f1e07489
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-24 16:18:06 +0530

    Added validation for existing email id ;

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -12,27 +12,40 @@ const register = (req, res, next) => {
     return;
   }
 
-  User.create(user)
-    .then((newUser) => {
-      const userDetails = {
-        username: newUser.username,
-        emailId: newUser.emailId,
-      };
-      res.status(201).json(userDetails);
-    })
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        error.status = 400;
-      } else {
-        error.status = 500;
-      }
-      res.status(error.status).json({
-        status: error.status,
-        message: error.message,
+  User.findOne({ emailId: user.emailId }, (error, user) => {
+    if (error) throw error;
+
+    if (user) {
+      res.status(400).json({
+        status: 400,
+        message: 'Email ID already taken. Please use a different Email Id',
       });
 
       return;
-    });
+    } else {
+      User.create(user)
+        .then((newUser) => {
+          const userDetails = {
+            username: newUser.username,
+            emailId: newUser.emailId,
+          };
+          res.status(201).json(userDetails);
+        })
+        .catch((error) => {
+          if (error.name === 'ValidationError') {
+            error.status = 400;
+          } else {
+            error.status = 500;
+          }
+          res.status(error.status).json({
+            status: error.status,
+            message: error.message,
+          });
+
+          return;
+        });
+    }
+  });
 };
 
 module.exports = {
