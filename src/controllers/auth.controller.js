f5c9e92a93247b9fa3370ec94f07f635f9f7de33
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-24 20:29:14 +0530

    Added login validation and authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const jwt = require('jsonwebtoken');
 
+// USER REGISTER
 const register = (req, res, next) => {
   const user = req.body;
 
@@ -92,6 +93,93 @@ const register = (req, res, next) => {
   });
 };
 
+// USER LOGIN
+const login = (req, res, next) => {
+  const user = req.body;
+
+  if (!user) {
+    res.status(401).json({
+      status: false,
+      message: 'User details not provided',
+    });
+    return;
+  }
+
+  if (!user.emailId || !user.password) {
+    res.status(401).json({
+      status: false,
+      message: 'Incorrect email id or password',
+    });
+    return;
+  }
+
+  User.findOne({ emailId: user.emailId }, (error, userFound) => {
+    if (error || !userFound) {
+      res.status(404).json({
+        status: false,
+        message: 'Credentials not found',
+      });
+      return;
+    } else {
+      userFound.checkPassword(user.password, (error, isMatch) => {
+        if (error || !isMatch) {
+          res.status(404).json({
+            status: false,
+            message: 'Credentials not found',
+          });
+          return;
+        }
+
+        const claims = {
+          username: userFound.username,
+          emailId: userFound.emailId,
+        };
+
+        // get from env variables
+        const secret = process.env.JWT_SECRET;
+        const expiry = Number(process.env.JWT_EXPIRES_IN);
+
+        jwt.sign(
+          claims,
+          secret,
+          {
+            expiresIn: expiry * 60 * 60, //expiry time in hours
+          },
+          (error, token) => {
+            if (error) {
+              // Throw error message
+              error.status = 500;
+              res.status(500).json({
+                status: false,
+                mesage: error.message,
+              });
+              return;
+            } else {
+              // set http cookie
+              res.cookie('jwt', token, {
+                expires: new Date(
+                  Date.now() + process.env.JWT_EXPIRES_IN * 60 * 60 * 1000
+                ),
+                httpOnly: false, //false - so that cookie can be deleted from client end
+              });
+              // Create new user on success and send as response
+              res.status(201).json({
+                status: true,
+                user: {
+                  username: userFound.username,
+                  emailId: userFound.emailId,
+                  token: token,
+                },
+              });
+            }
+          }
+        );
+      });
+    }
+  });
+};
+
 module.exports = {
   register,
+  login,
 };
