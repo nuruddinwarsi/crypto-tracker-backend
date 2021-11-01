3db123ef0a57dd8c7cd5108167e2c7078870563b
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-11-01 14:16:42 +0530

    set cookies jwt as token

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.cookies.jwt;
   if (!token) {
     res.status(401).json({
       status: false,
