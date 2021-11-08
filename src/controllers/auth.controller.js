e65324c57e1a37e9c936267775fadfc0f3d0ebbc
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-11-08 23:05:23 +0530

    auth - added specific error msg for login/register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,8 +7,9 @@ const register = (req, res, next) => {
   const user = req.body;
 
   if (!user || !user.username || !user.emailId || !user.password) {
-    res.status(400).json({
-      status: 400,
+    res.status(200).json({
+      code: 200,
+      status: false,
       message: 'User details missing',
     });
     return;
@@ -19,8 +20,9 @@ const register = (req, res, next) => {
 
     // Check if user already exists
     if (userFound) {
-      res.status(400).json({
-        status: 400,
+      res.status(200).json({
+        code: 200,
+        status: false,
         message: 'Email ID already taken. Please use a different Email Id',
       });
 
@@ -70,10 +72,12 @@ const register = (req, res, next) => {
                 );
                 // Create new user on success and send as response
                 res.status(201).json({
-                  status: 201,
+                  code: 201,
+                  status: true,
                   username: newUser.username,
                   emailId: newUser.emailId,
                   token: token,
+                  message: 'New user created',
                 });
               }
             }
@@ -101,16 +105,18 @@ const login = (req, res, next) => {
   const user = req.body;
 
   if (!user) {
-    res.status(401).json({
-      status: 401,
+    res.status(200).json({
+      code: 200,
+      status: false,
       message: 'User details not provided',
     });
     return;
   }
 
   if (!user.emailId || !user.password) {
-    res.status(401).json({
-      status: 401,
+    res.status(200).json({
+      code: 200,
+      status: false,
       message: 'Incorrect email id or password',
     });
     return;
@@ -118,17 +124,19 @@ const login = (req, res, next) => {
 
   User.findOne({ emailId: user.emailId }, (error, userFound) => {
     if (error || !userFound) {
-      res.status(404).json({
-        status: 404,
-        message: 'Credentials not found',
+      res.status(200).json({
+        code: 200,
+        status: false,
+        message: 'User not found',
       });
       return;
     } else {
       userFound.checkPassword(user.password, (error, isMatch) => {
         if (error || !isMatch) {
-          res.status(404).json({
-            status: 404,
-            message: 'Credentials not found',
+          res.status(200).json({
+            code: 200,
+            status: false,
+            message: 'Incorrect email id or password',
           });
           return;
         }
@@ -170,10 +178,12 @@ const login = (req, res, next) => {
               );
               // Create new user on success and send as response
               res.status(200).json({
-                status: 200,
+                code: 200,
+                status: true,
                 username: userFound.username,
                 emailId: userFound.emailId,
                 token: token,
+                message: 'Login successful',
               });
             }
           }
