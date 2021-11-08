dcd56f5b60a62a1fff1992f6db553fd1d61e339c
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-11-08 23:33:52 +0530

    auth - changes status codes for middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,7 +5,8 @@ const authenticate = (req, res, next) => {
     ? req.headers.authorization
     : req.cookies.jwt;
   if (!token) {
-    res.status(401).json({
+    res.status(200).json({
+      code: 200,
       status: false,
       message: 'Token missing',
     });
@@ -16,7 +17,8 @@ const authenticate = (req, res, next) => {
   const secret = process.env.JWT_SECRET;
   jwt.verify(token, secret, async (error, claims) => {
     if (error) {
-      res.status(401).json({
+      res.status(200).json({
+        code: 200,
         status: false,
         message: 'You are not authorized to access this resource.',
       });
